b621483719b762daffc6939ffb1ba7417c6718c9
Khaled Yassen <40730865+KayvT@users.noreply.github.com>
2020-08-13 13:08:33 +0300

    minor adjustments

--- src/Components/Landing/Clients.js
+++ src/Components/Landing/Clients.js
@@ -4,29 +4,61 @@ import MERCEDESicon from "../../Assets/clients/mercedeslogo.png";
 import THYicon from "../../Assets/clients/thyssenlogo.png";
 import Tsystemsicon from "../../Assets/clients/tsystemslogo.png";
 import SAPicon from "../../Assets/clients/saplogo.png";
-
-class OurClients extends React.Component {
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+export default class SimpleSlider extends React.Component {
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
     return (
+      <>
+        <Slider {...settings}>
+          <div>
+            <img id="clienticon" src={SIEMENicon} alt="siemen" />
+          </div>
+          <div>
+            <img id="clienticon" src={THYicon} alt="siemen" />
+          </div>
+          <div>
+            <img id="clienticon" src={Tsystemsicon} alt="siemen" />
+          </div>
+          <div>
+            <img id="clienticon" src={SAPicon} alt="siemen" />
+          </div>
+          <div>
+            <img id="clienticon" src={MERCEDESicon} alt="siemen" />
+          </div>
+        </Slider>
+      </>
+    );
+  }
+}
+
+/* 
+
       <div>
-        <div className="row" style={{ marginRight: 0, marginLeft: 0 }}>
-          <div className="col-lg-6 d-flex flex-column justify-content-center align-items-center">
-            <div className="w-100">
-              <h1 className="clients-text">HAPPY CLIENTS</h1>
+        <div classNameName="row" style={{ marginRight: 0, marginLeft: 0 }}>
+          <div classNameName="col-lg-6 d-flex flex-column justify-content-center align-items-center">
+            <div classNameName="w-100">
+              <h1 classNameName="clients-text">HAPPY CLIENTS</h1>
             </div>
           </div>
         </div>
-        <div className="row clients" style={{ margin: 0 }}>
+        <div classNameName="row clients" style={{ margin: 0 }}>
           <img id="clienticon" src={SIEMENicon} alt="siemen" />
           <img id="clienticon" src={MERCEDESicon} alt="siemen" />
           <img id="clienticon" src={THYicon} alt="siemen" />
         </div>
-        <div className="row clients" style={{ margin: 0 }}>
+        <div classNameName="row clients" style={{ margin: 0 }}>
           <img id="clienticon" src={Tsystemsicon} alt="siemen" />
           <img id="clienticon" src={SAPicon} alt="siemen" />
         </div>
       </div>
-    );
-  }
-}
-export default OurClients;
+      
+      */
